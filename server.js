773e4be3d3044b73ae94f190b59850dd1121f864
Daniel Atkinson <hourd.tasa@gmail.com>
2016-08-23 19:11:16 +0100

    initial working version with only speed gauge in KPH

---
+++ server.js
@@ -0,0 +1,100 @@
+var OBDReader = require('bluetooth-obd');
+var btOBDReader = new OBDReader();
+var dataReceivedMarker = {};
+var OBDConnection = false;
+var lastODBReading = 0;
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+ 
+console.log('Booting...');
+btOBDReader.on('connected', function () {
+    OBDConnection = true;
+    console.log('Connected');
+    //this.requestValueByName("vss"); //vss = vehicle speed sensor 
+ 
+    this.addPoller("temp");
+    this.addPoller("load_pct");
+    this.addPoller("map");
+    this.addPoller("iat");
+    this.addPoller("baro");
+    this.addPoller("aat");
+    // Speed
+    this.addPoller("vss");
+    this.addPoller("rpm");
+    this.addPoller("throttlepos");
+    // Fuel
+    this.addPoller("frp");
+    this.addPoller("fli");
+    this.addPoller("shrtft13");
+    this.addPoller("longft13");
+    this.addPoller("shrtft24");
+    this.addPoller("longft24");
+    this.addPoller("fuelsys");
+    this.addPoller("enginefrate");
+ 
+    this.startPolling(1000); //Request all values each second. 
+});
+
+
+function handleVSS(data) {
+  io.sockets.emit('update_vss', data.value);
+}
+ 
+btOBDReader.on('dataReceived', function (data) {
+    console.log(data);
+    dataReceivedMarker = data;
+    if(typeof data.name !== 'undefined') {
+      switch(data.name) {
+        case 'vss':
+          handleVSS(data);
+          break;
+        default:
+          console.log('Default Data Handle');
+          break;
+      }
+    }
+});
+
+function maintainOBDConnection() {
+  if(!OBDConnection) { 
+    try {
+      // Use first device with 'obd' in the name 
+      console.log('Autoconnecting...');
+      //btOBDReader.autoconnect('obd');
+      btOBDReader.autoconnect('OBDII');
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  }
+  setTimeout(maintainOBDConnection, 1000);
+} 
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/jquery.js', function(req, res){
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+});
+app.get('/gauge.js', function(req, res){
+  res.sendFile(__dirname + '/lib/jquery-guage/jquery-gauge.min.js');
+});
+app.get('/gauge.css', function(req, res){
+  res.sendFile(__dirname + '/lib/jquery-guage/jquery-gauge.css');
+});
+
+setTimeout(maintainOBDConnection, 1000);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
